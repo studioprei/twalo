7493293d04f52e28e92185f79293a0eff75ad9d1
Klaas Speller <klaasspeller@gmail.com>
2010-08-09 16:35:16 +0200

    Include Raphael and jquery

--- config.ru
+++ config.ru
@@ -1,5 +1,3 @@
 require "lib/twalo"
 
-Sinatra::Application.set :root, ::File.dirname(__FILE__)
-
 run Twalo::App
--- lib/twalo.rb
+++ lib/twalo.rb
@@ -3,6 +3,11 @@ require "haml"
 
 module Twalo
   class App < Sinatra::Base
+    
+    set :root, File.join(File.dirname(__FILE__), '..')
+    set :public, File.join(File.dirname(__FILE__), '..', 'public')
+    set :views, File.join(File.dirname(__FILE__), '..', 'views')
+      
     get '/' do
       haml :index
     end
